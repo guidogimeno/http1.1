6862204787d6edf26635f494a5a0d6edc8803d62
guidogimeno <gimenoguido@gmail.com>
2025-06-25 00:34:54 -0300

    asdf

--- strings.h
+++ strings.h
@@ -4,7 +4,8 @@ typedef struct String {
 } String;
 
 String string(char const *text);
-String substring(Allocator *allocator, char const *text, u32 start, u32 end);
+// TODO: requiere realmente un allocator?? Podria ser un slice
+String substring(Allocator *allocator, char const *text, u32 start, u32 end); 
 
 u32 string_len(char const *text);
 
